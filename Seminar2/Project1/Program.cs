687e1472fa2782b2f4e871317fca5f2f5a6b86ac
Aryslan <yak.aryslan.1999@gmail.com>
2019-09-12 10:28:10 +0300

    Seminar 2

---
+++ Seminar2/Project1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Project1
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            uint n;
+            string line;
+            do
+            {
+                Console.Write("Enter fibonacci's number index: ");
+                line = Console.ReadLine();
+            } while (!uint.TryParse(line, out n));
+
+            Console.WriteLine("fib({0}) = {1}", n, GetFibNumber(n));
+        }
+
+        private static uint GetFibNumber(uint n)
+        {
+            double b = Math.Pow((1 + Math.Sqrt(5)) / 2, n) - Math.Pow((1 - Math.Sqrt(5)) / 2, n);
+            return (uint) (b / Math.Sqrt(5) + 0.5);
+        }
+    }
+}
---
+++ Seminar2/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿/* 
+Дисциплина: "Программирование"
+Группа: БПИ196/1
+Студент: Якшибаев Арыслан Азаматович
+Задача: Написать метод для вычисления площади и длины окружности, радиус которой задает вещественный параметр.
+    В основной программе, вводя значения радиуса, с помощью обращения к методу, вычислять площадь и длину окружности. 
+    При вводе применять метод double.TryParse() и проверять корректность введенного значения.
+    При выводе использовать форматную строку метода WriteLine(). 
+    Окончание работы программы - ввод нулевого или отрицательного значения радиуса.
+
+Дата: 2019.09.12
+*/
+
+using System;
+
+namespace Task2
+{
+    internal static class Program
+    {
+        private static void Calculate(in double radius, out double s, out double c)
+        {
+            s = Math.PI * Math.Pow(radius, 2);
+            c = 2 * Math.PI * radius;
+            return;
+        }
+
+        public static void Main(string[] args)
+        {
+            double r = 1, // Радиус
+                s, // Площадь круга
+                c; // Длина окружности
+            string str;
+
+            while (r > 0)
+            {
+                do
+                {
+                    Console.Write("Введите радиус: ");
+                    str = Console.ReadLine();
+                } while (!double.TryParse(str, out r));
+
+                if (r <= 0)
+                    return;
+
+                Calculate(r, out s, out c);
+
+                Console.WriteLine("Введённый радиус: {0:f3}.\nПлощадь круга: {1:f3}\nДлина окружности: {2:f3}", r, s,
+                    c);
+            }
+        }
+    }
+}
